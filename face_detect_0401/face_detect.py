55362c406113a6936e475870e59a1eacc7668f5e
jjmmll0727 <48875473+jjmmll0727@users.noreply.github.com>
2020-04-03 21:37:34 +0900

    0403
    흑인에 대한 이미지 처리는 나중에 해놀놓을께요 오츄알고리즘이랑 스페이스바 부탁드립니다.

--- face_detect_0401/face_detect.py
+++ face_detect_0401/face_detect.py
@@ -3,14 +3,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 import keyboard
 
-
-
 def Show_2Dhistogram():
 
-    target = cv2.imread('face2.jpg')
+    target = cv2.imread('TrippleFace.png')
     hsvt = cv2.cvtColor(target, cv2.COLOR_BGR2HSV) ## RGB-->HSV
     histt = cv2.calcHist([hsvt], [0, 1], None, [255, 255], [0, 180, 0, 256]) # row: H(색조) / col: S(채도)
     #'his'togram of 't'arget
+
     cv2.imshow('histogram', histt)
     cv2.waitKey(0) #원하는 H,S 값을 구하기 힘든 --> 흑백으로 나옴
 
@@ -21,14 +20,15 @@ def Show_2Dhistogram():
 
 def CalcBack():
     # 역투영
-    roi = cv2.imread('skin.png') # 원하는 곳의 이미지
+    roi = cv2.imread('skin1.png') # 원하는 곳의 이미지
     hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-    target = cv2.imread('face.jpg') # 타겟이미지_목표
+    target = cv2.imread('TrippleFace.png') # 타겟이미지_목표
     hsvt = cv2.cvtColor(target, cv2.COLOR_BGR2HSV)
-
     roihist = cv2.calcHist([hsv], [0, 1], None, [180, 256], [0, 180, 0, 256]) # 원하는 곳의 이미지의 히스토그램
     cv2.normalize(roihist, roihist, 0, 255, cv2.NORM_MINMAX) # 해당 히스토그램을 0,255 로 정규화
+
     calc = cv2.calcBackProject([hsvt], [0,1], roihist, [0,180,0,256],1) #타겟이미지에서 원하는 이미지의 히스토그램을 이용해서 추출
+
     cv2.imshow('calc',calc)
     cv2.waitKey(0)
     return calc
@@ -36,9 +36,9 @@ def CalcBack():
 #calc를 반환하는 CalcBack 함수와 같은 기능, 이미지 출력이 겹쳐서 imshow는 구현X / "return calc"만 구현
 def CalcBack_no_image():
     # 역투영
-    roi = cv2.imread('skin.png') # 원하는 곳의 이미지
+    roi = cv2.imread('skin1.png') # 원하는 곳의 이미지
     hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-    target = cv2.imread('face.jpg') # 타겟이미지_목표
+    target = cv2.imread('TrippleFace.png') # 타겟이미지_목표
     hsvt = cv2.cvtColor(target, cv2.COLOR_BGR2HSV)
 
     roihist = cv2.calcHist([hsv], [0, 1], None, [180, 256], [0, 180, 0, 256]) # 원하는 곳의 이미지의 히스토그램
